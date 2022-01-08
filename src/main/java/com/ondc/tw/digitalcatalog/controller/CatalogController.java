d6420d74d89d149a80b95f7f243112b7b1ea8581
Naman Sahni <naman.sahni@thoughtworks.com>
2022-01-08 13:46:39 +0530

    return master item list from search

--- src/main/java/com/ondc/tw/digitalcatalog/controller/CatalogController.java
+++ src/main/java/com/ondc/tw/digitalcatalog/controller/CatalogController.java
@@ -1,8 +1,11 @@
 package com.ondc.tw.digitalcatalog.controller;
 
+import com.ondc.tw.digitalcatalog.model.MasterItem;
 import com.ondc.tw.digitalcatalog.service.CatalogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class CatalogController {
@@ -11,12 +14,13 @@ public class CatalogController {
     private CatalogService catalogService;
 
     @PostMapping(path = "/catalog/item/add")
-    public void addItem(){
+    public void addItem() {
+
     }
 
     @GetMapping(path = "catalog/master/search")
-    public void searchItems(@RequestParam String query){
-        catalogService.searchItems(query);
+    public List<MasterItem> searchItems(@RequestParam String query) {
+        return catalogService.searchItems(query);
     }
 
 }
